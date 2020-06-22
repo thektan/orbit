9084daa6a4612301cb3ca03ba8b0228d7b979406
Kevin Tan <kevin.tan@liferay.com>
2020-06-21 20:41:02 -0700

    formatting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,9 @@ import * as admin from "firebase-admin";
 
 let serviceAccount: any;
 let storageBucket: string = "";
+
 const environment = functions.config().app.environment;
+
 if (environment === "staging") {
   serviceAccount = require("../serviceAccount-staging.json");
   storageBucket = "emails-show-8eb6f.appspot.com";
@@ -11,6 +13,7 @@ if (environment === "staging") {
   serviceAccount = require("../serviceAccount-production.json");
   storageBucket = "emails-show-prod.appspot.com";
 }
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: storageBucket,
